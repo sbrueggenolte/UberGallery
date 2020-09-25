706af84ccb83a7194f6f2780b1cf52414ceb7371
Sascha Brüggenolte <sb@netinventors.de>
2020-09-25 16:49:34 +0200

    Change password request mail address

--- resources/themes/rosa-gruppe/login.php
+++ resources/themes/rosa-gruppe/login.php
@@ -49,7 +49,7 @@
 
                     <!-- Remind Passowrd -->
                     <div id="formFooter">
-                        <a class="underlineHover" href="mailto:sbrueggenolte@posteo.de?subject=Hilfe, ich brauche ein neues Passwort">Passwort vergessen?</a>
+                        <a class="underlineHover" href="mailto:fotos-rosa-gruppe@hascha.de?subject=Hilfe, ich brauche ein neues Passwort">Passwort vergessen?</a>
                     </div>
 
                 </div>
