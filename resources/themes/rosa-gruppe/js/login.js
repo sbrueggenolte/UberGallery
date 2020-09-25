b055ec9696cef4b934a11a9a95dd7a1fbfd6fbc9
Sascha Brüggenolte <sb@netinventors.de>
2020-09-25 16:38:50 +0200

    Fix redirect after login and remove unused code

--- resources/themes/rosa-gruppe/js/login.js
+++ resources/themes/rosa-gruppe/js/login.js
@@ -5,71 +5,31 @@ $(document).on('submit', '#login-form',  function(e) {
 
     $('#formContent').removeClass('shake-effect');
 
-
     $.post('/loginCheck.php', {
         'password': $('#password').val(),
         'email': $('#email').val()
     }, function(response) {
+        console.log(1, response);
         $('#locked-message').slideUp();
         if (window.PasswordCredential) {
             var c = new PasswordCredential(e.target);
+
             return navigator.credentials.store(c);
         } else {
             return Promise.resolve(profile);
         }
+    }).then(function() {
         window.location.replace('/');
     }).fail(function(response) {
         if (423 === response.status) {
             $('#locked-minutes').html(response.responseText);
             $('#locked-message').slideDown();
+
             return;
         }
 
         $('#locked-message').slideUp();
         $('#formContent').addClass('shake-effect');
     });
-
-
-
-
-/*
-
-    // Try sign-in with AJAX
-    fetch('/loginCheck.php', {
-        method: 'POST',
-        body: new FormData(e.target),
-        credentials: 'include'
-    }).then(res => {
-        if (res.status == 200) {
-            return Promise.resolve();
-        } else {
-            return Promise.reject('Sign-in failed');
-        }
-    }).then(profile => {
-        // Instantiate PasswordCredential with the form
-        if (window.PasswordCredential) {
-            var c = new PasswordCredential({
-                id: $('#email').val(),
-                password: $('#password').val(),
-                name: 'Fotos Rosa Gruppe',
-                iconURL: 'https://rosa.hascha.de'
-            });
-
-            var result = navigator.credentials.store(c);
-            return result;
-        } else {
-            return Promise.resolve(profile);
-        }
-    }).then(profile => {
-
-        // console.log(profile);
-        // Successful sign-in
-        // if (profile) {
-        //     window.location.replace('/');
-        // }
-    }).catch(error => {
-        $('#formContent').addClass('shake-effect');
-    });
-*/
 
 });
